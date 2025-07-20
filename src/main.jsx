2e6c3c4899e4d2c02ee30de5bc11bf082c0d56fc
Bicksona <bickson2004@gmail.com>
2025-07-20 19:45:50 +0530

    page name changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Page from "./components/page";
+import Page from "./components/Page";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<Page />);
